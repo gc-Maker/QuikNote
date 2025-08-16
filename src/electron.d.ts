73937df3d352b3679db1cfb510618852b7a196d0
gc-Maker <71691194+gc-Maker@users.noreply.github.com>
2025-08-16 18:15:54 +0800

    Update electron.d.ts

--- src/electron.d.ts
+++ src/electron.d.ts
@@ -1,8 +1,12 @@
+import { NotesAction } from "./type/enum/NotesAction";
+import { NoteItem } from "./type/interface/NoteItem";
+
 declare global {
     interface Window {
         electronAPI: {
             minimize: () => void;
             createWindow: () => void;
+            modifyNotes: (type: NotesAction, payloads: NoteItem[]) => void;
         };
     }
 }
