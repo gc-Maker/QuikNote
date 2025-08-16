9cf9f9e6f35aa522c6b477e17586b1069c5963d4
gc-Maker <71691194+gc-Maker@users.noreply.github.com>
2025-08-16 18:12:20 +0800

    Update main.js

--- public/electron/main.js
+++ public/electron/main.js
@@ -2,7 +2,8 @@ const { app } = require("electron");
 const { bindIpcEvent } = require("./ipcMain");
 const { autoSave } = require("./utils/autoSave");
 const { createWindows, createDefaultWindow } = require("./utils/window");
-const { store } = require("./store");
+const { store } = require("./models/store");
+const fs = require("fs");
 const path = require("path");
 
 const localDataPath = path.join(app.getPath("userData"), "app-data.json");
@@ -10,16 +11,37 @@ let windows = [];
 let timer;
 // 创建窗口函数
 function main() {
+    const handleDefault = () => {
+        const defaultWindow = createDefaultWindow();
+        windows = [defaultWindow];
+        const bounds = defaultWindow.getBounds();
+        const { customId } = defaultWindow;
+        store.initData({
+            windowDatas: [
+                {
+                    bounds,
+                    customId,
+                },
+            ],
+            noteDatas: [],
+        });
+    };
+
     try {
-        store.initData(localDataPath);
-        const localData = store.getData();
-        windows = createWindows(localData);
+        const localData = fs.readFileSync(localDataPath, "utf8");
+        if (localData) {
+            store.initData(JSON.parse(localData));
+            windows = createWindows(store.getWindows());
+            console.log("if wuliangjie");
+        } else {
+            console.log("else wuliangjie");
+            handleDefault();
+        }
     } catch (error) {
-        console.log(error, "error");
-        windows = [createDefaultWindow()];
+        console.log("catch wuliangjie");
+        handleDefault();
     }
 
-    // win.setSkipTaskbar(false);
     bindIpcEvent(windows);
     timer = autoSave(windows, localDataPath);
 }
@@ -27,9 +49,6 @@ function main() {
 // 应用就绪后创建窗口
 app.whenReady().then(() => {
     main();
-    // globalShortcut.register("CommandOrControl+Shift+I", () => {
-    //     mainWindow.webContents.openDevTools();
-    // });
 });
 
 app.on("window-all-closed", () => {
