e8ee7f4e2ec5295dc4d60c255dd41a19bec81bd4
gc-Maker <71691194+gc-Maker@users.noreply.github.com>
2025-08-16 18:12:35 +0800

    Update preload.js

--- public/electron/preload.js
+++ public/electron/preload.js
@@ -3,4 +3,6 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("electronAPI", {
     minimize: () => ipcRenderer.send("minimize-window"), // 最小化
     createWindow: () => ipcRenderer.send("create-window"), // 创建新窗口
+    modifyNotes: (type, payloads) =>
+        ipcRenderer.send("modify-notes", type, payloads),
 });
