7039df20b4e414ee5dc4d2b5c8eea69c9fbceaf4
gc-Maker <71691194+gc-Maker@users.noreply.github.com>
2025-08-16 18:11:59 +0800

    Update ipcMain.js

--- public/electron/ipcMain.js
+++ public/electron/ipcMain.js
@@ -1,7 +1,9 @@
 const { ipcMain, Tray, Menu } = require("electron");
 const path = require("path");
 const { createDefaultWindow } = require("./utils/window");
-
+const { NoteAction } = require("./type/NoteAction");
+const { store } = require("./models/store");
+const { Note } = require("./models/Note");
 function bindIpcEvent(windows) {
     ipcMain.on("minimize-window", (e) => {
         const curWin = e.sender;
@@ -27,6 +29,28 @@ function bindIpcEvent(windows) {
 
     ipcMain.on("create-window", () => {
         windows.push(createDefaultWindow());
+    });
+
+    ipcMain.on("modify-notes", (e, type, payloads) => {
+        const customId = e.sender.customId;
+        switch (type) {
+            case NoteAction.ADD:
+                const window = store.getWindows().find((window) => {
+                    return window.customId === customId;
+                });
+                if (window) {
+                    const newNotes = store.getNotes().concat(
+                        payloads.map((payload) => {
+                            return new Note(payload);
+                        })
+                    );
+                    store.updateData("notes", newNotes);
+                    window.setNoteIds(newNotes.map((item) => item.getId()));
+                }
+                break;
+            default:
+                return;
+        }
     });
 }
 
